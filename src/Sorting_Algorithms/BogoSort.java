da4b48cf6d5e60906ba5f8e92a11e48144955c2a
la_w <wlaphutama@protonmail.com>
2020-09-21 09:15:28 +0200

    update

--- src/Sorting_Algorithms/BogoSort.java
+++ src/Sorting_Algorithms/BogoSort.java
@@ -2,6 +2,42 @@ package Sorting_Algorithms;
 
 public class BogoSort {
     static void bogoSort(double[] arr) {
+        while (!isSorted(arr)) {
+            shuffleAndSwap(arr);
+        }
+    }
 
+    static void shuffleAndSwap(double[] arr) {
+        for (int r = arr.length - 1; r > 0; --r) {
+            int i = (int) (Math.random() * r);
+            double temp = arr[i];
+            arr[i] = arr[r];
+            arr[r] = temp;
+        }
+    }
+
+    static boolean isSorted(double[] arr){
+        for (int i = 1; i < arr.length; ++i) {
+            if (arr[i] < arr[i-1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        double[] testArr = new double[]{2.3, 2, 6, 5.4, 7.8, 80, 4.5, 9};
+
+        System.out.println("Unsorted array: ");
+        for (double item : testArr) {
+            System.out.print(item + " ");
+        }
+
+        bogoSort(testArr);
+
+        System.out.println("\nSorted array: ");
+        for (double item : testArr) {
+            System.out.print(item + " ");
+        }
     }
 }
---
+++ src/Sorting_Algorithms/MergeSort.java
@@ -0,0 +1,14 @@
+package Sorting_Algorithms;
+
+public class MergeSort {
+    static void mergeSort(double[] arr) {
+
+    }
+
+    static void merge(double[] left, double[] right) {
+
+
+        
+
+    }
+}
