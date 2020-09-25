6d262507435f67787c99a962beaa3240540b33df
Madtaccoo <daniel.wilke@iesy.net>
2020-09-25 10:28:20 +0200

    Class to solve functions
    Class to solve a function for 0 and create a table to plot the function.
    
    Signed-off-by: Madtaccoo <daniel.wilke@iesy.net>

--- src/NumericalMathematics/Newton/Newton.java
+++ src/NumericalMathematics/Newton/Newton.java
@@ -2,6 +2,7 @@ package NumericalMathematics.Newton;
 
 public abstract class Newton
 {
+    private static final int PLOT_SCALE = 75;
 
     private static double fun_0(double x_value)
     {
@@ -80,6 +81,49 @@ public abstract class Newton
         return f;
     }
 
+    private static double controller(int chosen)
+    {
+        double cSolve = 0;
+        double x_abscissa_n = 0;
+        double x_abscissa_p = 0;
+
+        switch (chosen)
+        {
+            // func_0
+            case 0:
+                x_abscissa_n = -3;
+                x_abscissa_p = 3;
+                break;
+            // func_1
+            case 1:
+                // func_2
+            case 2:
+                x_abscissa_n = -3;
+                x_abscissa_p = 0;
+                break;
+            // func_5
+            case 3:
+                x_abscissa_n = -2;
+                x_abscissa_p = 0;
+                break;
+            // func_cos
+            case 4:
+                x_abscissa_n = -0.2;
+                x_abscissa_p = 0;
+                break;
+            // func_tan
+            case 5:
+                x_abscissa_n = -1;
+                x_abscissa_p = -0.5;
+                break;
+            default:
+                break;
+        }
+
+        cSolve = solve(x_abscissa_n, x_abscissa_p, chosen);
+
+        return cSolve;
+    }
 
     private static double solve(double x_abscissa_n, double x_abscissa_p, int chosen)
     {
@@ -89,6 +133,7 @@ public abstract class Newton
         double abscissa_mid = 0;
         double ordinate_mid = 0;
         double temp = 0;
+        double x_tol = 1e-15;
 
         ordinate_n = get_ordinate(x_abscissa_n, chosen);
         ordinate_p = get_ordinate(x_abscissa_p, chosen);
@@ -98,6 +143,51 @@ public abstract class Newton
             System.out.println("No intersection with x-axis!");
         }
 
+        if (x_tol > (x_abscissa_p - x_abscissa_n) / 2)
+        {
+
+        }
+
+        if ((ordinate_n * ordinate_p) == 0)
+        {
+            if (Math.abs(ordinate_n) == 0)
+            {
+                abscissa_nod = ordinate_n;
+            }
+            if (Math.abs(ordinate_p) == 0)
+            {
+                abscissa_nod = ordinate_p;
+            }
+
+        } else
+        {
+            while (Math.abs(x_abscissa_n - x_abscissa_p) > x_tol)
+            {
+                // calculating the median of x_abscissa_n and x_abscissa_p
+                abscissa_mid = (x_abscissa_n + x_abscissa_p) / 2.0;
+
+                // calculating the ordinate
+                ordinate_mid = get_ordinate(abscissa_mid, chosen);
+                ordinate_n = get_ordinate(x_abscissa_n, chosen);
+                ordinate_p = get_ordinate(x_abscissa_p, chosen);
+
+                if (ordinate_n > ordinate_p)
+                {
+                    temp = x_abscissa_n;
+                    x_abscissa_n = x_abscissa_p;
+                    x_abscissa_p = temp;
+                }
+
+                if (ordinate_mid > 0)
+                {
+                    x_abscissa_p = abscissa_mid;
+                } else
+                {
+                    x_abscissa_n = abscissa_mid;
+                }
+            }
+            abscissa_nod = abscissa_mid;
+        }
 
         return abscissa_nod;
     }
@@ -130,5 +220,29 @@ public abstract class Newton
                 break;
         }
         return ordinate;
+    }
+
+    // Creating a table to plot the function
+    public static double[][] plottingTable(double a, double b, int chosen)
+    {
+        double steps = 0;
+        double counter = a * 2;
+
+        double cord[][] = new double[2][PLOT_SCALE]; // cord[x-axis][y-axis]
+
+        steps = ((b - a) * 2) / PLOT_SCALE;
+
+        for (int i = 0; i < PLOT_SCALE; i++)
+        {
+            cord[0][i] = counter;
+            cord[1][i] = get_ordinate(counter, chosen);
+            counter += (2 * steps);
+        }
+        return cord;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(String.format("%.4f", controller(2)));
     }
 }
