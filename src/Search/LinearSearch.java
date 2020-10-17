f518b1ca3ef9c2bbb063f9bb5f526cf7a43dd7ab
la_w <wlaphutama@protonmail.com>
2020-10-17 11:38:45 +0200

    update

--- src/Search/LinearSearch.java
+++ src/Search/LinearSearch.java
@@ -1,9 +1,20 @@
 package Search;
 
+/**
+ * Implementation of the Linear Search, which searches
+ * for a given element in an array
+ * @author Wutthichai Laphutama
+ */
 public class LinearSearch {
-    public static int getIndex(double[] ls,double target){
-        for (int i = 0; i < ls.length; i++) {
-            if(ls[i]==target)
+    public static int searchTarget(double[] arr, double target){
+        /**
+         * @param arr inital array
+         * @param target
+         * @return location of the target otherwise @return -1
+         * if target is not in the array
+         */
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == target)
                 return i;
         }
         return -1;
