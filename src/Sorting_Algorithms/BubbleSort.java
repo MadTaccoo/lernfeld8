b46f6dca75bcd215f0dd725770632d1994d99782
la_w <wlaphutama@protonmail.com>
2020-10-17 11:25:40 +0200

    update

--- src/Sorting_Algorithms/BubbleSort.java
+++ src/Sorting_Algorithms/BubbleSort.java
@@ -1,11 +1,18 @@
 package Sorting_Algorithms;
 
 /**
- * An implementation of an optimized version of The Bubble Sort
+ * An implementation of a commonly known version of
+ * the Bubble Sort and an optimized version of The Bubble Sort
  * @author Wutthichai Laphutama
  */
 public abstract class BubbleSort {
     public static void bubbleSort(double[] arr) {
+        /**
+         * The initial array is stepped through from left to right.
+         * In each step the current element is compared with an adjacent
+         * element and then swapped if the criteria, in which they are to be sorted, is wrong.
+         *
+         */
         for(int i = arr.length - 1; i > 0; --i) {
             for (int j = 0; j < i; ++j) {
                 if (arr[j] > arr[j + 1]) {
@@ -19,11 +26,8 @@ public abstract class BubbleSort {
 
     public static void bubbleSortOptimized(double[] arr) {
         /**
-         * The initial array is stepped through from left to right.
-         * In each step the current element is compared with an adjacent
-         * element and then swapped if the criteria, in which they are to be sorted, is wrong.
-         *
-         * In case that some or all elements in the given array are already in their final position, i.e.
+         * Works the same as the previous version, but in case that some
+         * or all elements in the given array are already in their final position, i.e.
          * already sorted, they do not need to be checked again.
          *
          * @param arr initial unsorted array, which is going to be sorted
@@ -45,6 +49,7 @@ public abstract class BubbleSort {
     }
 
 
+    /* For testing purposes */
     private static void main(String[] args) {
 
         double[] testArr = new double[]{2.3, 2, 6, 5.4, 7.8, 80, 4.5, 9};
