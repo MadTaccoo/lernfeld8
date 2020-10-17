844dcbdf4167d7d05fbf8f58cfca23db03fd7f9f
la_w <wlaphutama@protonmail.com>
2020-10-17 11:58:29 +0200

    update

--- src/Sorting_Algorithms/BogoSort.java
+++ src/Sorting_Algorithms/BogoSort.java
@@ -1,13 +1,27 @@
 package Sorting_Algorithms;
 
+/**
+ * Implementation of the Bogo Sort,
+ * where the array is shuffled at random until the array are in order
+ * @author Wutthichai Laphutama
+ */
 public abstract class BogoSort {
     public static void bogoSort(double[] arr) {
+        /**
+         * Checks whether array is sorted with isSorted-function
+         * if not call shuffleAndSwap
+         * @param arr array of type double, which needs to be sorted
+         */
         while (!isSorted(arr)) {
             shuffleAndSwap(arr);
         }
     }
 
     public static void shuffleAndSwap(double[] arr) {
+        /**
+         * Step through array, while shuffling and swapping its elements
+         * @param arr array of type double
+         */
         for (int r = arr.length - 1; r > 0; --r) {
             int i = (int) (Math.random() * r);
             double temp = arr[i];
@@ -17,6 +31,12 @@ public abstract class BogoSort {
     }
 
     public static boolean isSorted(double[] arr){
+        /**
+         * Check if given array is sorted
+         * @param arr array of type double
+         * @return false if value at index i is smaller than the preceding index i-1
+         * otherwise return true
+         */
         for (int i = 1; i < arr.length; ++i) {
             if (arr[i] < arr[i-1]) {
                 return false;
@@ -25,7 +45,8 @@ public abstract class BogoSort {
         return true;
     }
 
-    public static void main(String[] args) {
+    /* For testing purposes */
+    private static void main(String[] args) {
         double[] testArr = new double[]{2.3, 2, 6, 5.4, 7.8, 80, 4.5, 9};
 
         System.out.println("Unsorted array: ");
