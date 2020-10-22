abea15e37ab7577c4a6f2d6a269873d0d02e633c
la_w <wlaphutama@protonmail.com>
2020-10-22 11:59:02 +0200

    update

--- src/Graph/Dijkstra.java
+++ src/Graph/Dijkstra.java
@@ -51,7 +51,7 @@ public class Dijkstra {
 
 
     public static void main(String[] args) {
-        int graph[][] = new int[][] { {0, 2, 3},
+        int[][] graph = new int[][] { {0, 2, 3},
                                       {1, 0, 0},
                                       {0, 4, 5}};
         dijsktra_algorithm(graph, 1);
