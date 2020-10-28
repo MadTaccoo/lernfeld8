6c3fd56b3e3ca6e6841a9fcb1025d87c6d6c7c80
la_w <wlaphutama@protonmail.com>
2020-10-28 11:07:58 +0100

    Merge branch 'master' of https://github.com/MadTaccoo/lernfeld8 into djkstra_comments
     Conflicts:
    	src/Graph/Dijkstra_Algorithm.java

--- src/Sorting_Algorithms/BogoSort.java
+++ src/Sorting_Algorithms/BogoSort.java
@@ -3,25 +3,29 @@ package Sorting_Algorithms;
 /**
  * Implementation of the Bogo Sort,
  * where the array is shuffled at random until the array are in order
+ *
+ * Time Complexity of Bogo Sort:
+ * unbounded, since the shuffling is randomized; Best performance would be O(n),
+ * assuming the array is already sorted
+ *
  * @author Wutthichai Laphutama
  */
 public abstract class BogoSort {
+    /**
+     * Runs as long as array is not sorted yet
+     * @param arr (double[] arr) array that is going to be sorted
+     */
     public static void bogoSort(double[] arr) {
-        /**
-         * Checks whether array is sorted with isSorted-function
-         * if not call shuffleAndSwap
-         * @param arr array of type double, which needs to be sorted
-         */
         while (!isSorted(arr)) {
             shuffleAndSwap(arr);
         }
     }
 
+    /**
+     * Step through array, while shuffling and swapping its elements
+     * @param arr array of type double
+     */
     public static void shuffleAndSwap(double[] arr) {
-        /**
-         * Step through array, while shuffling and swapping its elements
-         * @param arr array of type double
-         */
         for (int r = arr.length - 1; r > 0; --r) {
             int i = (int) (Math.random() * r);
             double temp = arr[i];
@@ -30,13 +34,13 @@ public abstract class BogoSort {
         }
     }
 
+    /**
+     * Check if given array is sorted
+     * @param arr array of type double
+     * @return false if value at index i is smaller than the preceding index i-1
+     * otherwise return true
+     */
     public static boolean isSorted(double[] arr){
-        /**
-         * Check if given array is sorted
-         * @param arr array of type double
-         * @return false if value at index i is smaller than the preceding index i-1
-         * otherwise return true
-         */
         for (int i = 1; i < arr.length; ++i) {
             if (arr[i] < arr[i-1]) {
                 return false;
