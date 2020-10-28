6b32a0ea99be04bdb528ca52dff91680119e11a1
la_w <wlaphutama@protonmail.com>
2020-10-28 10:33:52 +0100

    Merge branch 'master' of https://github.com/MadTaccoo/lernfeld8 into djkstra_comments
     Conflicts:
    	src/Graph/Dijkstra_Algorithm.java

--- src/Sorting_Algorithms/BubbleSort.java
+++ src/Sorting_Algorithms/BubbleSort.java
@@ -3,16 +3,23 @@ package Sorting_Algorithms;
 /**
  * An implementation of a commonly known version of
  * the Bubble Sort and an optimized version of The Bubble Sort
+ *
+ * Complexity of Bubble Sort:
+ * For each element in the array, the bubble Sort does n - 1 comparisons.
+ * This means the comparison takes O(n) time. And since the array has n elements, we can
+ * say that there are O(n) number of elements. This means the bubble sort performs O(n) operations
+ * on O(n) elements, leading to a time complexity of O(n^2)
+ *
  * @author Wutthichai Laphutama
  */
 public abstract class BubbleSort {
+    /**
+     * The initial array is stepped through from left to right.
+     * In each step the current element is compared with an adjacent
+     * element and then swapped if the criteria, in which they are to be sorted, is wrong.
+     * @param arr (double[]) array, which is going to be sorted
+     */
     public static void bubbleSort(double[] arr) {
-        /**
-         * The initial array is stepped through from left to right.
-         * In each step the current element is compared with an adjacent
-         * element and then swapped if the criteria, in which they are to be sorted, is wrong.
-         *
-         */
         for(int i = arr.length - 1; i > 0; --i) {
             for (int j = 0; j < i; ++j) {
                 if (arr[j] > arr[j + 1]) {
@@ -24,14 +31,14 @@ public abstract class BubbleSort {
         }
     }
 
+    /**
+     * Works the same as the previous version, but in case that some
+     * or all elements in the given array are already in their final position, i.e.
+     * already sorted, they do not need to be checked again.
+     *
+     * @param arr initial unsorted array, which is going to be sorted
+     */
     public static void bubbleSortOptimized(double[] arr) {
-        /**
-         * Works the same as the previous version, but in case that some
-         * or all elements in the given array are already in their final position, i.e.
-         * already sorted, they do not need to be checked again.
-         *
-         * @param arr initial unsorted array, which is going to be sorted
-         */
         int n = arr.length;
         boolean swapped;    /* checks whether elements in the array need to be swapped */
         do {
