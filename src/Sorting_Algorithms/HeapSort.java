9066dfd985f9b4f103f22a35c1abc4ec9e8f3110
la_w <wlaphutama@protonmail.com>
2020-10-28 13:31:56 +0100

    Merge branch 'master' of https://github.com/MadTaccoo/lernfeld8 into djkstra_comments
     Conflicts:
    	src/Graph/Dijkstra_Algorithm.java

--- src/Sorting_Algorithms/HeapSort.java
+++ src/Sorting_Algorithms/HeapSort.java
@@ -5,14 +5,27 @@ package Sorting_Algorithms;
  * The goal is to find the largest value in the array/list and store it at the end.
  * These steps are repeated until every element is sorted
  *
+ * Complexity of Heap Sort:
+ * Creating the Heap takes O(n) calls of the maxHeapify-function.
+ * The maxHeapify-function in itself takes O(log n) times. Therefore
+ * the total complexity is O(n log n).
  *
  * @author Wutthichai Laphutama
  */
 public abstract class HeapSort {
+    /**
+     * When the root does not meet the heap property (root is larger than its children or
+     * does not have children), swap until the criteria is met.
+     * @param arr (double[]) array, that is going to be sorted
+     * @param heapSize (int) size of the heap
+     * @param parent (int) parent node
+     */
     private static void maxHeapify(double[] arr, int heapSize, int parent) {
+        /* Searching for the largest value in the parent node, left child and right child */
         int largest = parent;
-        int leftChild = 2 * parent + 1;
-        int rightChild = 2 * parent + 2;
+        int leftChild = 2 * parent + 1; /* to access the left child from the parent node*/
+        int rightChild = 2 * parent + 2; /* to access the right child from the parent node*/
+
 
         if (leftChild < heapSize && arr[leftChild] > arr[largest]) {
             largest = leftChild;
@@ -22,6 +35,7 @@ public abstract class HeapSort {
             largest = rightChild;
         }
 
+        /* If root is not the largest value, swap until it is the largest value */
         if (largest != parent) {
             double temp = arr[parent];
             arr[parent] = arr[largest];
@@ -31,6 +45,10 @@ public abstract class HeapSort {
         }
     }
 
+    /**
+     * Creation of the heap
+     * @param arr (double[]) array, that is going to be sorted
+     */
     private static void createHeap(double[] arr) {
         int heapLength = arr.length;
         for (int i = heapLength / 2 - 1; i >= 0; --i) {
@@ -38,10 +56,17 @@ public abstract class HeapSort {
         }
     }
 
+    /**
+     * Creates a heap and then sorts all elements of
+     * the array
+     * @param arr (double[]) array, that is going to be sorted
+     */
     public static void heapSort(double[] arr) {
         int heapLength = arr.length;
         createHeap(arr);
 
+        /* Stores the largest element at the end and decreases the range of values
+        int the heap by one */
         for (int i = heapLength - 1; i >= 0; --i) {
             double temp = arr[0];
             arr[0] = arr[i];
