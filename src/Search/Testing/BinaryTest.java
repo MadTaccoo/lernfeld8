ea59a4b54845a5bb703900373c829d44db414232
Jan-Frederik Reuter <Jan-Frederik@gmx.net>
2021-03-09 12:02:09 +0100

    d

--- src/Search/Testing/BinaryTest.java
+++ src/Search/Testing/BinaryTest.java
@@ -4,36 +4,54 @@ import Database.MySqlCon;
 import Search.BinarySearch;
 import Search.LinearSearch;
 import org.junit.jupiter.api.*;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class BinaryTest {
     static double[] listtoSearchIn;
-    static double valueToFind;
-    static int index;
+    static double[] valuesToFind;
+    static int[] indexes;
 
     @BeforeAll
     public static void setUp(){
-        ArrayList<String> directData = MySqlCon.query("SELECT * FROM tbl_searchTest ORDER BY data");
+
+        ArrayList<String> directData = MySqlCon.query(
+                "SELECT * FROM tbl_searchTest ORDER BY data");
+
+        ArrayList<String> results = MySqlCon.query(
+                "SELECT data FROM tbl_searchResults");
+
+        ArrayList<String> indexData = MySqlCon.query(
+                "SELECT indexVal FROM tbl_searchResults");
+
         assert directData != null;
+        assert results != null;
+        assert indexData != null;
+
         directData.remove(0);
+        results.remove(0);
+        indexData.remove(0);
+
         listtoSearchIn = directData.stream().mapToDouble(Double::parseDouble).toArray();
-        index = 10;
-        valueToFind = listtoSearchIn[index];
+        valuesToFind = results.stream().mapToDouble(Double::parseDouble).toArray();
+        indexes = indexData.stream().mapToInt(Integer::parseInt).toArray();
+
         System.out.println("..");
     }
 
-    @RepeatedTest(10)
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4})
     @DisplayName("Test Binary Search")
-    public void testBinarySearch(){
-        Assertions.assertEquals(index,BinarySearch.binarySearch(listtoSearchIn,valueToFind));
-        Assertions.assertEquals(index,BinarySearch.binarySearchR(listtoSearchIn,valueToFind));
+    public void testBinarySearch(int i){
+        Assertions.assertEquals(indexes[i],BinarySearch.binarySearch(listtoSearchIn,valuesToFind[i]));
+        Assertions.assertEquals(indexes[i],BinarySearch.binarySearchR(listtoSearchIn,valuesToFind[i]));
     }
 
-    @RepeatedTest(10)
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4})
     @DisplayName("Test Linear Search")
-    public void testLinearSearch(){
-        Assertions.assertEquals(index,LinearSearch.searchTarget(listtoSearchIn,valueToFind));
+    public void testLinearSearch(int i){
+        Assertions.assertEquals(indexes[i],LinearSearch.searchTarget(listtoSearchIn,valuesToFind[i]));
     }
 }
